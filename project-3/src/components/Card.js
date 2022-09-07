36c14ac6bbf0b2b22b0bf079ad882715c593b069
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-07 15:04:04 +0300

    added the Card component

---
+++ project-3/src/components/Card.js
@@ -0,0 +1,19 @@
+export default function Card(props) {
+  const { image, icon, rating, reviewCount, country, title, price } = props;
+  
+  return (
+    <section className="cards">
+      <img className="cards--pic" src={image} alt="Olympic Swimmer" />
+      <div>
+        <img className="cards--star" src={icon} alt="" />
+        <p className="cards--text">
+          {rating} <span className="text--location">({reviewCount}) • {country}</span>
+        </p>
+      </div>
+      <p className="cards--text">{title}</p>
+      <p className="cards--price">
+        <strong>From ${price}</strong> / person
+      </p>
+    </section>
+  );
+}
