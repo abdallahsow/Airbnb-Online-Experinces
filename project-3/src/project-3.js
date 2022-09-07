029b6185c14dc6871c36f64ceebd328066deff07
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-07 15:06:45 +0300

    updated the project-3.js file

---
+++ project-3/src/project-3.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import data from "./components/data";
+import { Navbar, Pictures, Advertisement } from "./components/HeroSection";
+import Card from "./components/Card";
+
+const App = () => (
+  <main className="app">
+    <Navbar />
+    <div className="experiences">
+      <Pictures />
+      <Advertisement />
+      <div className="cards-data">
+        {data.map(({ img, star, rating, reviews, location, title, price }) => (
+          <Card
+            image={img}
+            icon={star}
+            rating={rating}
+            reviewCount={reviews}
+            country={location}
+            title={title}
+            price={price}
+          />
+        ))}
+      </div>
+    </div>
+  </main>
+);
+
+export default App;
